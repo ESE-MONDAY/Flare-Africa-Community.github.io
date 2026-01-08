1cd390f50eee047ffdadc5d7e6c6bc6a031f7266
Flare Africa Community <flareafrican@gmail.com>
2026-01-08 22:50:39 +0100

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,12 +8,21 @@ export async function GET(context) {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
+		xmlns: {
+	      atom: 'http://www.w3.org/2005/Atom',
+	      dc: 'http://purl.org/rss/1.0/modules/dc/',
+	    },
+		customData: `
+	      <language>en-us</language>
+	      <atom:link href="${new URL('rss.xml', context.site)}" rel="self" type="application/rss+xml" />
+	    `,
 		items: posts.map((post) => ({
 			title: post.data.title,
 			pubDate: post.data.date,
 			description: post.data.description,
 			link: `/blog/${post.slug}/`,
-			categories: [post.data.author],
+			categories: [post.data.author, "Flare Network", "Web3"],
+			customData: `<dc:creator>${post.data.author}</dc:creator>`,
 		})),
 	});
 }
