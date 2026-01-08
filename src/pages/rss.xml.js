29a2aec8e1afe6ac975a00d236b222576eaddb9d
Flare Africa Community <flareafrican@gmail.com>
2026-01-08 23:03:35 +0100

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,26 +3,24 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../const';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		xmlns: {
-	      atom: 'http://www.w3.org/2005/Atom',
-	      dc: 'http://purl.org/rss/1.0/modules/dc/',
-	    },
-		customData: `
-	      <language>en-us</language>
-	      <atom:link href="${new URL('rss.xml', context.site)}" rel="self" type="application/rss+xml" />
-	    `,
-		items: posts.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.date,
-			description: post.data.description,
-			link: `/blog/${post.slug}/`,
-			categories: [post.data.author, "Flare Network", "Web3"],
-			customData: `<dc:creator>${post.data.author}</dc:creator>`,
-		})),
-	});
+  const posts = await getCollection('blog');
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    xmlns: {
+      atom: 'http://www.w3.org/2005/Atom',
+      dc: 'http://purl.org/rss/1.0/modules/dc/',
+    },
+    customData: `<language>en-us</language>
+      <atom:link href="${new URL('rss.xml', context.site)}" rel="self" type="application/rss+xml" />`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+      categories: [post.data.author, "Flare Network", "Web3"],
+      customData: `<dc:creator>${post.data.author}</dc:creator>`,
+    })),
+  });
 }
